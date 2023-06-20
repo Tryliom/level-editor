4803ddb4ba5bf615d4a1af7ab1a8a554ea2ed61c
Tryliom <alexishaldy@gmail.com>
2023-06-20 11:34:20 +0200

    Update

--- build_web.bat
+++ build_web.bat
@@ -2,7 +2,7 @@ call C:\tools\emsdk\emsdk_env.bat
 
 mkdir bin
 
-call C:\tools\emsdk\upstream\emscripten\em++ src\Displays\Editor.cpp src\Displays\MainMenu.cpp src\Displays\SnakeGame.cpp src\Grid.cpp src\main.cpp libs\Basic\src\AudioManager.cpp libs\Basic\src\Image.cpp libs\Basic\src\Input.cpp libs\Basic\src\Timer.cpp libs\Basic\src\Utility.cpp libs\Basic\src\Window.cpp libs\Basic\src\Display.cpp libs\Basic\src\DisplayManager.cpp ^
+call C:\tools\emsdk\upstream\emscripten\em++ src\Displays\Editor.cpp src\Displays\MainMenu.cpp src\Displays\SnakeGame.cpp src\Grid.cpp src\main.cpp libs\Basic\src\AudioManager.cpp libs\Basic\src\Image.cpp libs\Basic\src\Input.cpp libs\Basic\src\Timer.cpp libs\Basic\src\Utility.cpp libs\Basic\src\Window.cpp libs\Basic\src\Display.cpp libs\Basic\src\DisplayManager.cpp libs\Basic\src\Button.cpp ^
     -o bin/game.js ^
     -std=c++20 ^
     -Wall -Wextra ^
@@ -29,5 +29,3 @@ call C:\tools\emsdk\upstream\emscripten\em++ src\Displays\Editor.cpp src\Display
     --no-entry
 
 cd bin
-
-python3 -m http.server 8000
